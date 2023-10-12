e24097487260c182c5b67ada8fc6fc150b295770
maoxin53 <maoxin53@gmail.com>
2023-10-12 17:50:29 +0200

    first site

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import {
+  bootstrapCameraKit,
+  createMediaStreamSource,
+  Transform2D,
+} from "@snap/camera-kit";
+
+(async function () {
+  const cameraKit = await bootstrapCameraKit({
+    apiToken:
+      "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk3MTI0MTMyLCJzdWIiOiJkZDgyOTBhYS1jY2YyLTRjNzEtODIxYy1hNDEzYWQ1OWQ5YTh-UFJPRFVDVElPTn5mZGQxZjY1Yi0yMThjLTQwMWMtYmZkNC1iNDU2MWE5Njg4NjQifQ.8H4gw_GWNU6FPj2o9nTqWkDg0mqwJg2lL6LH5681GnM",
+  });
+  const session = await cameraKit.createSession();
+  document.getElementById("canvas").replaceWith(session.output.live);
+
+  const { lenses } = await cameraKit.lensRepository.loadLensGroups([
+    "2a3368ce-84a3-45de-9abb-603b948e02ed",
+  ]);
+  session.applyLens(lenses[0]);
+
+  let mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+  const source = createMediaStreamSource(mediaStream, {
+    transform: Transform2D.MirrorX,
+    cameraType: "front",
+  });
+
+  await session.setSource(source);
+  session.source.setRenderSize(window.innerWidth, window.innerHeight);
+
+  session.play();
+})();
